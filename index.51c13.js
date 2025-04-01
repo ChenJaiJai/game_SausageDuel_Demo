3a932d9ac328113cc23caee0b569335fc47fab9e
JaiJai <wm.rd01@gmail.com>
2025-04-01 11:11:33 +0800

    0.0.2

--- index.51c13.js
+++ index.51c13.js
@@ -1,4 +1,4 @@
-System.register(["./application.23957.js"], function (_export, _context) {
+System.register(["./application.65cb0.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
